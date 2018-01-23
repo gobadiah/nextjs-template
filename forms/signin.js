e86cd66c588d8a7cc04992a2251341c93f557b40
Michaël Journo <gobadiah@gmail.com>
2018-01-23 23:25:29 +0100

    Clean up SignInForm

--- forms/signin.js
+++ forms/signin.js
@@ -2,28 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
 
-const SignInForm = (props) => {
-  const { handleSubmit } = props;
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="email">Email</label>
-        <Field name="email" component="input" type="email" />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <Field name="password" component="input" type="password" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+const SignInForm = ({ handleSubmit, error }) => (
+  <form onSubmit={handleSubmit}>
+    <div>
+      <label htmlFor='email'>Email</label>
+      <Field
+        name='email'
+        component='input'
+        type='email'
+        autocomplete='email'
+      />
+    </div>
+    <div>
+      <label htmlFor='password'>Password</label>
+      <Field
+        name='password'
+        component='input'
+        type='password'
+        autocomplete='current-password'
+      />
+    </div>
+    { error ? <div>{error}</div> : undefined }
+    <button type='submit'>Submit</button>
+  </form>
+);
 
 SignInForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.arraOf(PropTypes.string),
+};
+
+SignInForm.defaultProps = {
+  error: [],
 };
 
 export default reduxForm({
-  // a unique name for the form
   form: 'signin',
 })(SignInForm);
